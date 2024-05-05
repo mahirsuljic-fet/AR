786d658736cd89a7d6b3861d2f7ba8648147ee66
mahirsuljic-fet <mahir.suljic@fet.ba>
2024-05-05 15:03:04 +0200

    v6 p2 main.s komentari

--- Vjezbe/v6/p2/main.s
+++ Vjezbe/v6/p2/main.s
@@ -11,16 +11,21 @@ main:
   addiu $sp, $sp, -32
   sw $ra, 28($sp)
 
+  # double spremamo na taj nacin da druga 4 bajta
+  # idu u memoriju prije prva 4 bajta, "naopako"
+  # ovo se radi jer koristimo little endian sistem
+
   lui $t1, 0x4024
   ori $t1, $t1, 0x4CCC
   lui $t0, 0xCCCC
   ori $t0, $t0, 0xCCCD
 
-  sw $t0, 20($sp)
-  sw $t1, 24($sp)
+  sw $t0, 16($sp)
+  sw $t1, 20($sp)
 
-  addiu $a0, $sp, 20
+  addiu $a0, $sp, 16
 
+  # i ovdje ucitajemo broj "naopako"
   lui $a3, 0x402E
   ori $a3, $a3, 0x3333
   lui $a2, 0x3333
@@ -29,8 +34,10 @@ main:
   jal foo
   
   la $a0, print_str
-  lw $a2, 20($sp)
-  lw $a3, 24($sp)
+  # $a1 je "prazan" (nije bitna vrijednost) 
+  # jer je prvi argument pointer a drugi argument double
+  lw $a2, 16($sp)
+  lw $a3, 20($sp)
 
   jal printf
 
